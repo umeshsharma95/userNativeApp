36928d7302f65f9fc84bbdce73b29a39d7e522f2
Umesh Sharma <umeshs@iprogrammer.com>
2023-04-12 16:25:19 +0530

    removed test case

--- __tests__/Home-test.js
+++
@@ -1,9 +0,0 @@
-import "react-native";
-import React from "react";
-import Home from "../containers/Home";
-import renderer from "react-test-renderer";
-
-test("Home page snapshot", () => {
-  const snap = renderer.create(<Home />).toJSON();
-  expect(snap).toMatchSnapshot();
-});
